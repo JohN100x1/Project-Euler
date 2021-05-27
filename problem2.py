ce1d03460f38f86bd9df41aae929d5246a578f52
John Kong <JohN100x1@hotmail.co.uk>
2021-05-27 05:10:55 +0100

    Initial commit

---
+++ problem2.py
@@ -0,0 +1,12 @@
+def fibonacci_even_sum(n):
+    # n is the n-th even term in the fibonacci sequence
+    # There are 11 even terms before fibonacci > 4 million
+    # Use closed form for fibonacci term
+    # Apply geometric series sum
+    phi3 = (0.5*(1+5**0.5))**3
+    psi3 = (0.5*(1-5**0.5))**3
+    # Fn = (phi**n - psi**n)/5**0.5
+    Sn = (phi3*(1-phi3**n)/(1-phi3)-psi3*(1-psi3**n)/(1-psi3))/5**0.5
+    return Sn
+
+print(fibonacci_even_sum(11))
---
+++ problem3.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 27 05:07:34 2021
+
+@author: JohN100x1
+"""
+
