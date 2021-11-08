4f22ccd3b4f800a5a72d9eca7a8818ff0e36f52c
John Kong <JohN100x1@hotmail.co.uk>
2021-11-08 16:47:38 +0000

    v2.6

---
+++ problem071.py
@@ -0,0 +1,17 @@
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+def get_max_numerator_37():
+    max_n = 0
+    max_nd = 0.0
+    for d in range(10**6, 4, -1):
+        for n in range((3*d-1)//7,(3*d)//7+1):
+            if 7*n < 3*d and gcd(n, d) == 1:
+                if n/d > max_nd:
+                    max_n = n
+                    max_nd = n/d
+    return max_n
+
+print(get_max_numerator_37())
