72d91f93122fd15519ab009da0a653843475f695
John Kong <JohN100x1@hotmail.co.uk>
2021-11-29 21:26:58 +0000

    v3.2 - minor fixes

--- problem076.py
+++ problem076.py
@@ -1,9 +1,13 @@
-def get_partition_count(n0, n):
-    if n == 1:
-        return 0
-    count = 0
-    for n1, n2 in enumerate(range(n-1-n0,n//2-(1-n%2),-1),n0):
-        count += 1 + get_partition_count(n1, n2)
-    return count
+def get_partition_count(n):
+    plist = [1, 1]
+    for x in range(2, n+1):
+        psum = 0
+        sqrt1p24x = (1+24*x)**0.5
+        for k in range(1, int(1+sqrt1p24x)//6+1):
+            psum += (-1)**(k+1)*plist[x-k*(3*k-1)//2]
+        for k in range(1, int(-1+sqrt1p24x)//6+1):
+            psum += (-1)**(k+1)*plist[x-k*(3*k+1)//2]
+        plist.append(psum)
+    return plist[n]-1
 
-print(get_partition_count(0, 100))
+print(get_partition_count(100))
--- problem078.py
+++ problem078.py
@@ -1,12 +1,15 @@
-def partition(n):
-    if n <= 0:
-        return 0
-    elif n in {1,2,3}:
-        return n
-    else:
+def get_partition_divisible_by(d, maxit=10**6):
+    plist = [1, 1]
+    for x in range(2, maxit+1):
         psum = 0
-        k = 1
-        while n > k*(3*k-1)//2:
-            psum += (-1)**(k+1)*(partition(n-k*(3*k-1)//2)+partition(n-k*(3*k+1)//2))
-            k += 1
-        return psum
+        sqrt1p24x = (1+24*x)**0.5
+        for k in range(1, int(1+sqrt1p24x)//6+1):
+            psum += (-1)**(k+1)*plist[x-k*(3*k-1)//2]
+        for k in range(1, int(-1+sqrt1p24x)//6+1):
+            psum += (-1)**(k+1)*plist[x-k*(3*k+1)//2]
+        if psum % d == 0:
+            return x
+        plist.append(psum)
+    return None
+
+print(get_partition_divisible_by(10**6))
