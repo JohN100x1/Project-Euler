06f573941b9ea6755c18778faa46d36154f43157
JohN100x1 <JohN100x1@hotmail.co.uk>
2022-02-05 03:03:22 +0000

    0.1.1

--- src/solutions/p011.py
+++ src/solutions/p011.py
@@ -1,35 +1,42 @@
 import numpy as np
 
-GRID = np.loadtxt("p011_grid.txt", dtype=np.int64)
+from config import path_res
 
-def get_largest_prod():
-    product = 1
-    n = GRID.shape[0]
+
+def get_largest_prod() -> int:
+
+    # Load from file
+    grid = np.loadtxt(path_res / "p011_grid.txt", dtype=np.int64)
+
+    max_product = 1
+    n = grid.shape[0]
+
     # row product
     for i in range(n):
-        for j in range(n-4):
-            p = np.prod(GRID[i,j:j+4])
-            if p >= product:
-                product = p
+        for j in range(n - 4):
+            p = np.prod(grid[i, j : j + 4])
+            if p >= max_product:
+                max_product = p
+
     # column product
-    for i in range(n-4):
+    for i in range(n - 4):
         for j in range(n):
-            p = np.prod(GRID[i:i+4,j])
-            if p >= product:
-                product = p
-    dx = np.array([0,1,2,3])
+            p = np.prod(grid[i : i + 4, j])
+            if p >= max_product:
+                max_product = p
+    dx = np.array([0, 1, 2, 3])
+
     # left diagonal product
-    for i in range(n-4):
-        for j in range(n-4):
-            p = np.prod(GRID[i+dx,j+dx])
-            if p >= product:
-                product = p
+    for i in range(n - 4):
+        for j in range(n - 4):
+            p = np.prod(grid[i + dx, j + dx])
+            if p >= max_product:
+                max_product = p
+
     # right diagonal product
     for i in range(3, n):
-        for j in range(n-4):
-            p = np.prod(GRID[i-dx,j+dx])
-            if p >= product:
-                product = p
-    return int(product)
-
-print(get_largest_prod())
+        for j in range(n - 4):
+            p = np.prod(grid[i - dx, j + dx])
+            if p >= max_product:
+                max_product = p
+    return max_product
---
+++ tests/test_p011.py
@@ -0,0 +1,5 @@
+from solutions.p011 import get_largest_prod
+
+
+def test_get_largest_prod():
+    assert get_largest_prod() == 70600674
