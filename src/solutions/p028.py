dd2f11711b85932ed79f7648b78abf4b0df2b8bf
JohN100x1 <JohN100x1@hotmail.co.uk>
2022-03-26 18:04:20 +0000

    0.1.12

--- src/solutions/p028.py
+++ src/solutions/p028.py
@@ -1,6 +1,7 @@
-def get_ulam_spiral_diag_sum(n):
-    # https://www.desmos.com/calculator/eqr1s8kxcn
-    S = (n+1)*(4*n**2-n+9)//6-3
-    return S
-
-print(get_ulam_spiral_diag_sum(1001))
+def sum_diagonals_ulam_spiral(n: int) -> int:
+    """
+    Get the sum of the numbers on a nxn Ulam spiral.
+    https://www.desmos.com/calculator/eqr1s8kxcn
+    """
+    diagonal_sum = (n + 1) * (4 * n**2 - n + 9) // 6 - 3
+    return diagonal_sum
---
+++ tests/test_solutions/test_p028.py
@@ -0,0 +1,5 @@
+from solutions.p028 import sum_diagonals_ulam_spiral
+
+
+def test_sum_diagonals_ulam_spiral():
+    assert sum_diagonals_ulam_spiral(1001) == 669171001
