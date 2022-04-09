647fb4568c7a8cda0eadf4a1091e369b3dfe067f
JohN100x1 <JohN100x1@hotmail.co.uk>
2022-04-09 13:29:03 +0100

    0.1.42

--- src/solutions/p063.py
+++ src/solutions/p063.py
@@ -1,11 +1,14 @@
-def get_nth_digit_power_count():
+def count_nth_digit_power() -> int:
+    """
+    Get the number of n-digit numbers which are n-th powers.
+
+    https://www.desmos.com/calculator/ol6twhcock
+    """
     count = 0
     n = 1
-    lbound = pow(10,1-1/n)
+    lbound = pow(10, 1 - 1 / n)
     while lbound <= 9:
-        count += 10+int(-lbound//1)
+        count += 10 + int(-lbound // 1)
         n += 1
-        lbound = pow(10,1-1/n)
+        lbound = pow(10, 1 - 1 / n)
     return count
-
-print(get_nth_digit_power_count())
